6dac48b468b402c05a08f18631880069aafdd50e
luave <lucyavedisyan2002@gmail.com>
2024-10-17 18:13:55 +0300

    update launch settings

--- ProductCatalogApp/Program.cs
+++ ProductCatalogApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ProductCatalogApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,15 +11,24 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Добавление сервисов в контейнер
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Конфигурация HTTP запроса
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        // Настройка UI Swagger, для открытия сразу по URL
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+        options.RoutePrefix = string.Empty;  // Чтобы Swagger был доступен по корневому пути, http://localhost:5000
+    });
 }
 
 app.UseHttpsRedirection();
